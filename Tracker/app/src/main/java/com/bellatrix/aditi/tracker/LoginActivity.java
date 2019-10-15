474ee9fb100d497a1a0b0f13d09e15b22164227b
Aditi Bansal <bansaladiti5@gmail.com>
2019-10-14 17:37:54 -0700

    Migrated to android 9

--- Tracker/app/src/main/java/com/bellatrix/aditi/tracker/LoginActivity.java
+++ Tracker/app/src/main/java/com/bellatrix/aditi/tracker/LoginActivity.java
@@ -3,12 +3,13 @@ package com.bellatrix.aditi.tracker;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.CountDownTimer;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.bellatrix.aditi.tracker.Utils.CommonFunctions;
 import com.thrivecom.ringcaptcha.RingcaptchaAPIController;
