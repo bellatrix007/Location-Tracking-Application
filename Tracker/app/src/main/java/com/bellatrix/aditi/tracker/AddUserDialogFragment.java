62150c8069ee68c8ed7b1eaa155d09f81a85cbc6
Aditi Bansal <bansaladiti5@gmail.com>
2019-10-15 21:50:42 -0700

    UI for add user

--- Tracker/app/src/main/java/com/bellatrix/aditi/tracker/AddUserDialogFragment.java
+++ Tracker/app/src/main/java/com/bellatrix/aditi/tracker/AddUserDialogFragment.java
@@ -9,9 +9,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.DialogFragment;
 
 import com.google.firebase.FirebaseApp;
@@ -27,12 +30,17 @@ public class AddUserDialogFragment extends DialogFragment {
     private FirebaseDatabase firebaseDatabase;
     private DatabaseReference databaseReference;
 
+    private Button neutral, positive;
+    private EditText et_user_phonenumber;
+    private TextView tv_user_text;
+    private ImageView iv_user_sign;
+
     @Override
     public Dialog onCreateDialog(final Bundle savedInstanceState) {
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
-        builder.setTitle("New Folder");
+        builder.setTitle("Add User");
         LayoutInflater inflater = getActivity().getLayoutInflater();
         builder.setView(inflater.inflate(R.layout.dialog_add_user, null))
                 .setPositiveButton("Search", null)
@@ -40,47 +48,100 @@ public class AddUserDialogFragment extends DialogFragment {
                     public void onClick(DialogInterface dialog, int id) {
                         AddUserDialogFragment.this.getDialog().cancel();
                     }
-                });
+                })
+                .setNeutralButton("Back", null);
+
         final AlertDialog mAlertDialog = builder.create();
+
         mAlertDialog.setOnShowListener(new DialogInterface.OnShowListener() {
 
             @Override
             public void onShow(DialogInterface dialog) {
 
-                Button b = mAlertDialog.getButton(AlertDialog.BUTTON_POSITIVE);
-                b.setOnClickListener(new View.OnClickListener() {
+                neutral = mAlertDialog.getButton(AlertDialog.BUTTON_NEUTRAL);
+                positive = mAlertDialog.getButton(AlertDialog.BUTTON_POSITIVE);
+                et_user_phonenumber = ((EditText) AddUserDialogFragment.this.getDialog()
+                        .findViewById(R.id.et_user_number));
+                tv_user_text = ((TextView) AddUserDialogFragment.this.getDialog()
+                        .findViewById(R.id.tv_user_text));
+                iv_user_sign = ((ImageView) AddUserDialogFragment.this.getDialog()
+                        .findViewById(R.id.iv_user_sign));
+
+                // neutral button would be invisible initially
+                neutral.setVisibility(View.GONE);
+
+                neutral.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        neutral.setVisibility(View.GONE);
+                        et_user_phonenumber.setVisibility(View.VISIBLE);
+                        et_user_phonenumber.setText("");
+                        tv_user_text.setVisibility(View.GONE);
+                        iv_user_sign.setVisibility(View.GONE);
+                        positive.setText("Search");
+                    }
+                });
+
+                positive.setOnClickListener(new View.OnClickListener() {
 
                     @Override
                     public void onClick(View view) {
 
-                        String user_phonenumber =  ((EditText) AddUserDialogFragment.this.getDialog()
-                                .findViewById(R.id.et_user_number))
-                                .getText().toString();
-                        // search for the user in the database
-                        firebaseDatabase = FirebaseDatabase.getInstance();
-                        databaseReference = firebaseDatabase.getReference();
-                        Query searchQuery = databaseReference.child("users").orderByKey().equalTo(user_phonenumber);
-                        searchQuery.addValueEventListener(new ValueEventListener() {
-                            @Override
-                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                                if(dataSnapshot.exists()) {
-                                    //Key exists
-                                    Log.d("User","User exists");
-                                } else {
-                                    Log.d("User","User does not exist");
-                                }
-                            }
+                        if(positive.getText().toString().equals("Search"))
+                        {
+                            searchForUser();
+                        }
+                        else
+                        {
+                            // add user, send request
 
-                            @Override
-                            public void onCancelled(@NonNull DatabaseError databaseError) {
+                            // dismiss dialog
+                            AddUserDialogFragment.this.getDialog().dismiss();
+                        }
 
-                            }
-                        });
                     }
                 });
             }
         });
 
         return mAlertDialog;
+    }
+
+    private void searchForUser() {
+
+        // search for the user in the database
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
+        Query searchQuery = databaseReference.child("users")
+                .orderByKey().equalTo(et_user_phonenumber.getText().toString());
+        searchQuery.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                neutral.setVisibility(View.VISIBLE);
+                et_user_phonenumber.setVisibility(View.GONE);
+                tv_user_text.setVisibility(View.VISIBLE);
+                iv_user_sign.setVisibility(View.VISIBLE);
+                positive.setText("Add");
+
+                if(dataSnapshot.exists()) {
+                    //Key exists
+                    // TODO: Also print the user name
+                    Log.d("User","User exists");
+                    tv_user_text.setText("User verified");
+                    iv_user_sign.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.ic_tick, null));
+
+                } else {
+                    Log.d("User","User does not exist");
+                    tv_user_text.setText("User does not exist");
+                    iv_user_sign.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.ic_cross, null));
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
