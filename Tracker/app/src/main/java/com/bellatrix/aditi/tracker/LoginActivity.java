eb1655784480b84cbb5445538e809e2998e39096
xxator <parth.agarwal131@gmail.com>
2019-10-16 14:41:46 +0530

    Shared pref debugged(like a pro)

--- Tracker/app/src/main/java/com/bellatrix/aditi/tracker/LoginActivity.java
+++ Tracker/app/src/main/java/com/bellatrix/aditi/tracker/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.CountDownTimer;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -137,7 +138,8 @@ public class LoginActivity extends AppCompatActivity {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         SharedPreferences.Editor spEdit = sp.edit();
-        spEdit.putString("user", phone);
+        if(sp.getString("user","").equals(""))
+            spEdit.putString("user", phone);
         spEdit.putBoolean("logged", true);
         spEdit.apply();
         finish();
